2eceae89cc4b1009cbe6481e3bd59866db46a52e
Yamuna <hyamuna2807@gmail.com>
2026-04-20 21:34:52 +0530

    Add files via upload

---
+++ tuples3.py
@@ -0,0 +1,12 @@
+my_tuple=("1,2,3,4,5,6,7")
+my_tuple2=("8,9,10,11,12,13,14,15")
+print(my_tuple + my_tuple2)
+d="Domi"
+e="nos"
+print(d+e)
+house="Rental."
+rent="rent is 5k per month."
+print(house+rent)
+alpha="a,b,c,d,e,f,g,h,"
+alpha2="i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"
+print(alpha+alpha2)
